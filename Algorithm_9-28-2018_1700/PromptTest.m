1234bd198e0ffdf79258780dc73aad808c204572
Cesar <rasecf22@yahoo.com>
2018-10-10 16:07:28 -0600

    Removed Unnecessary Files

--- Algorithm_9-28-2018_1700/PromptTest.m
+++
@@ -1,9 +0,0 @@
-prompt = {'Enter matrix size:','Enter colormap name:'};
-title = 'Input';
-dims = [1 35];
-definput = {'20','hsv'};
-answer = inputdlg(prompt,title,dims,definput)
-
-
-h = answer(1,1)
-b = str2double(h)
